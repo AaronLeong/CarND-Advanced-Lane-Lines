1e530e46bf74942f8200597a3c05295ee3b5aace
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2017-01-21 20:02:42 -0800

    Calibration

---
+++ calibration.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+from tqdm import tqdm
+
+def calculate_mtx_dist(images, nx = 9, ny = 6, verbose = False):
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((ny*nx,3), np.float32)
+    objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d points in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    # Step through the list and search for chessboard corners
+
+    imgs = images
+
+    if verbose:
+      imgs = tqdm(images)
+
+    for idx, fname in enumerate(imgs):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chessboard corners
+        ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
+
+#         print('ret =', ret)
+
+        # If found, add object points, image points
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            # Draw and display the corners
+#             cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
+#             write_name = 'output_images/corners_found/corners_found'+str(idx)+'.jpg'
+#             cv2.imwrite(write_name, img)
+    #         cv2.imshow('img', img)
+    #         cv2.waitKey(500)
+
+    # Test undistortion on an image
+    # img = cv2.imread('camera_cal/calibration1.jpg')
+    img = cv2.imread(images[0])
+    img_size = (img.shape[1], img.shape[0])
+
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+
+    return mtx, dist
---
+++ camera-calibration.py
@@ -0,0 +1,17 @@
+'''
+  Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
+'''
+
+import cv2
+import argparse
+import numpy as np
+
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Compute calibration matrix and distortion coefficients")
+
+  print("Hello World!")
+
+if __name__ == '__main__':
+  main()
